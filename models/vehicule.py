61baf1e7a026129de58912886cb55362711482b9
Ton nom <kubra.kurnaz@etu.univ-tours.fr>
2026-02-13 14:14:01 +0100

    "KURNAZ- 04-serialisation-client-vehicule"

--- models/vehicule.py
+++ models/vehicule.py
@@ -18,3 +18,24 @@ class Vehicule:
     def __str__(self) -> str:
         km = int(self.kilometrage_actuel)
         return f"{self.id_vehicule} - {self.marque} {self.modele} ({self.cylindree} cyl., {km} km)"
+
+    def to_dict(self) -> dict:
+        return {
+            "id_vehicule": self.id_vehicule,
+            "marque": self.marque,
+            "modele": self.modele,
+            "cylindree": self.cylindree,
+            "kilometrage_actuel": self.kilometrage_actuel,
+            "date_mise_en_circulation": self.date_mise_en_circulation,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Vehicule":
+        return cls(
+            id_vehicule=data["id_vehicule"],
+            marque=data["marque"],
+            modele=data["modele"],
+            cylindree=int(data["cylindree"]),
+            kilometrage_actuel=float(data["kilometrage_actuel"]),
+            date_mise_en_circulation=data["date_mise_en_circulation"],
+        )
